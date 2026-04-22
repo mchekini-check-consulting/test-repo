e326d75d088691040aa6e9e2472f6fbff448e79c
mchekini <me.chekini@gmail.com>
2026-04-22 16:12:03 +0200

    build: update configuration

--- src/main/java/com/example/demo/MyFirstController.java
+++ src/main/java/com/example/demo/MyFirstController.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyFirstController {
+
+
+    @GetMapping("tester")
+    public String tester() {
+        return "hello world";
+    }
 }
