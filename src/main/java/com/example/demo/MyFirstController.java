aaf3dc5edcabe543f430b7456b59ee54dec87cf7
mchekini <me.chekini@gmail.com>
2026-04-22 23:06:31 +0200

    feat: add a new endpoint

--- src/main/java/com/example/demo/MyFirstController.java
+++ src/main/java/com/example/demo/MyFirstController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class MyFirstController {
     @GetMapping("tester")
     public String tester() {
         return "hello world";
+    }
+
+
+    @PostMapping("python")
+    public String python() {
+        return "hello world from python";
     }
 }
